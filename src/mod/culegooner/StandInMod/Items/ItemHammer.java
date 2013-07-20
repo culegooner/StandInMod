b3df2325ff75f83850e61996ebed015333df609d
culegooner <culegooner@server.fake>
2013-07-20 19:02:39 +0300

    wip

--- src/mod/culegooner/StandInMod/Items/ItemHammer.java
+++ src/mod/culegooner/StandInMod/Items/ItemHammer.java
@@ -13,6 +13,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.Direction;
 import net.minecraft.util.MathHelper;
 import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraft.item.ItemPickaxe;
 
@@ -34,20 +35,14 @@ public class ItemHammer extends ItemPickaxe {
 	@SideOnly(Side.CLIENT)
 	public void registerIcons(IconRegister iconRegister) {
 
-		itemIcon = iconRegister.registerIcon(ModLib.MOD_ID.toLowerCase()
-				+ ":"
-				+ this.getUnlocalizedName().substring(
-						this.getUnlocalizedName().indexOf(".") + 1));
+		itemIcon = iconRegister.registerIcon(ModLib.MOD_ID.toLowerCase() + ":" + this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".") + 1));
 	}
 
-	static Material[] materials = new Material[] { Material.rock,
-			Material.iron, Material.ice, Material.glass, Material.piston,
-			Material.anvil, Material.grass, Material.ground, Material.sand,
-			Material.snow, Material.craftedSnow, Material.clay };
-
 	@Override
-	public boolean onBlockStartBreak(ItemStack itemstack, int X, int Y, int Z,
-			EntityPlayer player) {
+	public boolean onBlockStartBreak(ItemStack itemstack, int X, int Y, int Z, EntityPlayer player) {
+
+		if (player.capabilities.isCreativeMode)
+			return super.onBlockStartBreak(itemstack, X, Y, Z, player);
 
 		final int blockID = player.worldObj.getBlockId(X, Y, Z);
 		final int meta = player.worldObj.getBlockMetadata(X, Y, Z);
@@ -57,23 +52,15 @@ public class ItemHammer extends ItemPickaxe {
 
 		float blockHardness = block.getBlockHardness(player.worldObj, X, Y, Z);
 
-		boolean validStart = false;
-		for (int iter = 0; iter < materials.length; iter++) {
-			if (materials[iter] == block.blockMaterial) {
-				validStart = true;
-				break;
-			}
-		}
-
-		MovingObjectPosition movingobjectposition = this
-				.getMovingObjectPositionFromPlayer(player.worldObj, player,
-						true);
-
-		if (movingobjectposition == null || !validStart)
+		if (!this.canHarvestBlock(block))
 			return super.onBlockStartBreak(itemstack, X, Y, Z, player);
 
-		// player.addChatMessage("X: " + X + " Y: " + Y + " Z: " + Z +
-		// " movingobjectposition.side " + movingobjectposition.sideHit);
+		MovingObjectPosition movingobjectposition = this.getMovingObjectPositionFromPlayer(player.worldObj, player, true);
+
+		if (movingobjectposition == null)
+			return super.onBlockStartBreak(itemstack, X, Y, Z, player);
+
+		// player.addChatMessage("X: " + X + " Y: " + Y + " Z: " + Z + " movingobjectposition.side " + movingobjectposition.sideHit);
 		int xRange = 1;
 		int yRange = 1;
 		int zRange = 1;
@@ -116,8 +103,7 @@ public class ItemHammer extends ItemPickaxe {
 		}
 
 		/**
-		 * Which side was hit. If its -1 then it went the full length of the ray
-		 * trace. Bottom = 0, Top = 1, East = 2, West = 3, North = 4, South = 5.
+		 * Which side was hit. If its -1 then it went the full length of the ray trace. Bottom = 0, Top = 1, East = 2, West = 3, North = 4, South = 5.
 		 */
 
 		// Direction.directions[i5]
@@ -127,11 +113,8 @@ public class ItemHammer extends ItemPickaxe {
 		// 3 East
 
 		if (this.itemModName == ItemsInit.WOODHAMMER_NAME) {
-			int i5 = MathHelper
-					.floor_double((double) (player.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
-			// player.addChatMessage("sideHit " + movingobjectposition.sideHit +
-			// " i5: " + i5 + " X: " + xRange + " Y: " + yRange + " Z: " +
-			// zRange);
+			int i5 = MathHelper.floor_double((double) (player.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+			// player.addChatMessage("sideHit " + movingobjectposition.sideHit + " i5: " + i5 + " X: " + xRange + " Y: " + yRange + " Z: " + zRange);
 
 			switch (i5) {
 			case 0:
@@ -156,35 +139,28 @@ public class ItemHammer extends ItemPickaxe {
 					if (xPos == X && yPos == Y && zPos == Z)
 						continue;
 
-					int localblockID = player.worldObj.getBlockId(xPos, yPos,
-							zPos);
+					int localblockID = player.worldObj.getBlockId(xPos, yPos, zPos);
 					block = Block.blocksList[localblockID];
-					int localMeta = player.worldObj.getBlockMetadata(xPos,
-							yPos, zPos);
-					int hlvl = MinecraftForge.getBlockHarvestLevel(block, meta,
-							"pickaxe");
-					float localHardness = block == null ? Float.MAX_VALUE
-							: block.getBlockHardness(player.worldObj, xPos,
-									yPos, zPos);
+					int localMeta = player.worldObj.getBlockMetadata(xPos, yPos, zPos);
+					// int hlvl = MinecraftForge.getBlockHarvestLevel(block, meta, "pickaxe");
+					float localHardness = block == null ? Float.MAX_VALUE : block.getBlockHardness(player.worldObj, xPos, yPos, zPos);
 
-					if (block != null && !(localHardness < 0)) {
-						for (int iter = 0; iter < materials.length; iter++) {
-							if (materials[iter] == block.blockMaterial) {
-								if (!player.capabilities.isCreativeMode) {
-									block.harvestBlock(player.worldObj, player,
-											xPos, yPos, zPos, localMeta);
-									block.onBlockHarvested(player.worldObj, X,
-											Y, Z, localMeta, player);
-									if (blockHardness > 0f)
-										onBlockDestroyed(itemstack,
-												player.worldObj, localblockID,
-												xPos, yPos, zPos, player);
-								}
-								player.worldObj.setBlockToAir(xPos, yPos, zPos);
+					if (block != null && !(localHardness < 0.0F)) {
+						if (!player.capabilities.isCreativeMode) {
+							if (this.canHarvestBlock(block)) {
+								block.harvestBlock(player.worldObj, player, xPos, yPos, zPos, localMeta);
+
+								block.onBlockHarvested(player.worldObj, X, Y, Z, localMeta, player);
+
+								if (blockHardness > 0.0F)
+									this.onBlockDestroyed(itemstack, player.worldObj, localblockID, xPos, yPos, zPos, player);
+
+								block.removeBlockByPlayer(player.worldObj, player, xPos, yPos, zPos);
 							}
-						}
 
+						}
 					}
+
 				}
 			}
 		}
