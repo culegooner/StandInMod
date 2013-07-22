0f5050303c9339fad59cecb9657af6cada0377b4
culegooner <culegooner@server.fake>
2013-07-23 02:00:56 +0300

    spade

---
+++ src/mod/culegooner/StandInMod/Items/ItemPaddle.java
@@ -0,0 +1,187 @@
+package mod.culegooner.StandInMod.Items;
+
+import mod.culegooner.StandInMod.ModLib;
+import mod.culegooner.StandInMod.ModMain;
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.EnumToolMaterial;
+import net.minecraft.item.ItemSpade;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.MathHelper;
+import net.minecraft.util.MovingObjectPosition;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class ItemPaddle extends ItemSpade {
+
+	protected String itemModName;
+
+	public ItemPaddle(int id, String name, EnumToolMaterial par3EnumToolMaterial) {
+		super(id - ModLib.SHIFTED_ID_RANGE_CORRECTION, par3EnumToolMaterial);
+		itemModName = name;
+
+		this.setCreativeTab(ModMain.tabMod);
+		this.setUnlocalizedName(itemModName);
+		// maxStackSize = 1;
+		// setNoRepair();
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister iconRegister) {
+
+		itemIcon = iconRegister.registerIcon(ModLib.MOD_ID.toLowerCase() + ":" + this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".") + 1));
+	}
+
+	@Override
+	public boolean onBlockStartBreak(ItemStack itemstack, int X, int Y, int Z, EntityPlayer player) {
+
+		if (player.capabilities.isCreativeMode)
+			return super.onBlockStartBreak(itemstack, X, Y, Z, player);
+
+		final int blockID = player.worldObj.getBlockId(X, Y, Z);
+		final int meta = player.worldObj.getBlockMetadata(X, Y, Z);
+		Block block = Block.blocksList[blockID];
+		if (block == null)
+			return super.onBlockStartBreak(itemstack, X, Y, Z, player);
+
+		float blockHardness = block.getBlockHardness(player.worldObj, X, Y, Z);
+
+		
+		if (ItemsInit.CLEARALLBLOCKS == false) {
+			if (canShovel(block) <= 1.0F)
+				return super.onBlockStartBreak(itemstack, X, Y, Z, player);
+		}
+		
+
+		MovingObjectPosition movingobjectposition = this.getMovingObjectPositionFromPlayer(player.worldObj, player, true);
+
+		if (movingobjectposition == null)
+			return super.onBlockStartBreak(itemstack, X, Y, Z, player);
+
+		// player.addChatMessage("X: " + X + " Y: " + Y + " Z: " + Z + " movingobjectposition.side " + movingobjectposition.sideHit);
+		int xRange = 1;
+		int yRange = 1;
+		int zRange = 1;
+
+		if (this.itemModName == ItemsInit.COBBLEPADDLE_NAME) {
+			xRange = 1;
+			yRange = 1;
+			zRange = 1;
+		} else if (this.itemModName == ItemsInit.IRONPADDLE_NAME) {
+			xRange = 1;
+			yRange = 1;
+			zRange = 1;
+		} else if (this.itemModName == ItemsInit.GOLDPADDLE_NAME) {
+			xRange = 1;
+			yRange = 1;
+			zRange = 1;
+		} else if (this.itemModName == ItemsInit.DIAMONDPADDLE_NAME) {
+			xRange = 2;
+			yRange = 2;
+			zRange = 2;
+		} else {
+			xRange = 1;
+			yRange = 1;
+			zRange = 1;
+		}
+
+		switch (movingobjectposition.sideHit) {
+		case 0:
+		case 1:
+			yRange = 0;
+			break;
+		case 2:
+		case 3:
+			zRange = 0;
+			break;
+		case 4:
+		case 5:
+			xRange = 0;
+			break;
+		}
+
+		/**
+		 * Which side was hit. If its -1 then it went the full length of the ray trace. Bottom = 0, Top = 1, East = 2, West = 3, North = 4, South = 5.
+		 */
+
+		// Direction.directions[i5]
+		// 0 South
+		// 1 West
+		// 2 North
+		// 3 East
+
+		if (this.itemModName == ItemsInit.WOODPADDLE_NAME) {
+			int i5 = MathHelper.floor_double((double) (player.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+			// player.addChatMessage("sideHit " + movingobjectposition.sideHit + " i5: " + i5 + " X: " + xRange + " Y: " + yRange + " Z: " + zRange);
+
+			switch (i5) {
+			case 0:
+				xRange = 0;
+				break;
+			case 1:
+				zRange = 0;
+				break;
+			case 2:
+				xRange = 0;
+				break;
+			case 3:
+				zRange = 0;
+				break;
+			}
+		}
+
+		//algorithm by mDiyo
+		
+		for (int xPos = X - xRange; xPos <= X + xRange; xPos++) {
+			for (int yPos = Y - yRange; yPos <= Y + yRange; yPos++) {
+				for (int zPos = Z - zRange; zPos <= Z + zRange; zPos++) {
+
+					if (xPos == X && yPos == Y && zPos == Z)
+						continue;
+
+					int localblockID = player.worldObj.getBlockId(xPos, yPos, zPos);
+					block = Block.blocksList[localblockID];
+					int localMeta = player.worldObj.getBlockMetadata(xPos, yPos, zPos);
+					//int hlvl = MinecraftForge.getBlockHarvestLevel(block, meta, "shovel");
+					float localHardness = block == null ? Float.MAX_VALUE : block.getBlockHardness(player.worldObj, xPos, yPos, zPos);
+
+					if (block != null && !(localHardness < 0.0F)) {
+
+						if ((this.canShovel(block) > 1.0F) || ItemsInit.CLEARALLBLOCKS) {
+							
+							block.harvestBlock(player.worldObj, player, xPos, yPos, zPos, localMeta);
+
+							block.onBlockHarvested(player.worldObj, X, Y, Z, localMeta, player);
+
+							if (blockHardness > 0.0F) {
+								if ((this.itemModName == ItemsInit.COBBLEPADDLE_NAME) || (this.itemModName == ItemsInit.WOODPADDLE_NAME))
+									this.onBlockDestroyed(itemstack, player.worldObj, localblockID, xPos, yPos, zPos, player);
+							}
+							block.removeBlockByPlayer(player.worldObj, player, xPos, yPos, zPos);
+						}
+
+					}
+
+				}
+			}
+		}
+
+		return false;
+	}
+
+	  public float canShovel(Block par2Block)
+	    {
+	        for (int i = 0; i < this.blocksEffectiveAgainst.length; ++i)
+	        {
+	            if (this.blocksEffectiveAgainst[i] == par2Block)
+	            {
+	                return this.efficiencyOnProperMaterial;
+	            }
+	        }
+
+	        return 1.0F;
+	    }
+	  
+}
--- src/mod/culegooner/StandInMod/Items/ItemsInit.java
+++ src/mod/culegooner/StandInMod/Items/ItemsInit.java
@@ -13,33 +13,64 @@ public class ItemsInit {
 	public static boolean CLEARALLBLOCKS;
 
 	public static final String CLEARALLBLOCKS_NAME = "clearAllBlocks";
+	
 	public static final String WOODHAMMER_NAME = "woodHammer";
 	public static final String COBBLEHAMMER_NAME = "cobbleHammer";
 	public static final String IRONHAMMER_NAME = "ironHammer";
 	public static final String GOLDHAMMER_NAME = "goldHammer";
 	public static final String DIAMONDHAMMER_NAME = "diamondHammer";
+	
 	public static final String FILLWAND_NAME = "fillWand";
+	
+	public static final String WOODPADDLE_NAME = "woodPaddle";
+	public static final String COBBLEPADDLE_NAME = "cobblePaddle";
+	public static final String IRONPADDLE_NAME = "ironPaddle";
+	public static final String GOLDPADDLE_NAME = "goldPaddle";
+	public static final String DIAMONDPADDLE_NAME = "diamondPaddle";
 
 	public static int WOODHAMMER_DEFAULT = ModLib.ITEMID_START + 1;
 	public static int COBBLEHAMMER_DEFAULT = ModLib.ITEMID_START + 2;
 	public static int IRONHAMMER_DEFAULT = ModLib.ITEMID_START + 3;
 	public static int GOLDHAMMER_DEFAULT = ModLib.ITEMID_START + 4;
 	public static int DIAMONDHAMMER_DEFAULT = ModLib.ITEMID_START + 5;
+	
 	public static int FILLWAND_DEFAULT = ModLib.ITEMID_START + 6;
 
+	public static int WOODPADDLE_DEFAULT = ModLib.ITEMID_START + 7;
+	public static int COBBLEPADDLE_DEFAULT = ModLib.ITEMID_START + 8;
+	public static int IRONPADDLE_DEFAULT = ModLib.ITEMID_START + 9;
+	public static int GOLDPADDLE_DEFAULT = ModLib.ITEMID_START + 10;
+	public static int DIAMONDPADDLE_DEFAULT = ModLib.ITEMID_START + 11;
+	
+	
 	public static int WOODHAMMER;
 	public static int COBBLEHAMMER;
 	public static int IRONHAMMER;
 	public static int GOLDHAMMER;
 	public static int DIAMONDHAMMER;
+	
 	public static int FILLWAND;
 
+	public static int WOODPADDLE;
+	public static int COBBLEPADDLE;
+	public static int IRONPADDLE;
+	public static int GOLDPADDLE;
+	public static int DIAMONDPADDLE;
+	
 	public static Item woodHammer;
 	public static Item cobbleHammer;
 	public static Item ironHammer;
 	public static Item goldHammer;
 	public static Item diamondHammer;
+	
 	public static Item fillWand;
+	
+	public static Item woodPaddle;
+	public static Item cobblePaddle;
+	public static Item ironPaddle;
+	public static Item goldPaddle;
+	public static Item diamondPaddle;
+	
 
 	public static void init() {
 
@@ -49,14 +80,29 @@ public class ItemsInit {
 		ironHammer = new ItemHammer(IRONHAMMER, IRONHAMMER_NAME, EnumToolMaterial.IRON);
 		goldHammer = new ItemHammer(GOLDHAMMER, GOLDHAMMER_NAME, EnumToolMaterial.GOLD);
 		diamondHammer = new ItemHammer(DIAMONDHAMMER, DIAMONDHAMMER_NAME, EnumToolMaterial.EMERALD);
+	
 		fillWand = new ItemWand(FILLWAND, FILLWAND_NAME);
-
+		
+		woodPaddle = new ItemPaddle(WOODPADDLE, WOODPADDLE_NAME, EnumToolMaterial.WOOD);
+		cobblePaddle = new ItemPaddle(COBBLEPADDLE, COBBLEPADDLE_NAME, EnumToolMaterial.STONE);
+		ironPaddle = new ItemPaddle(IRONPADDLE, IRONPADDLE_NAME, EnumToolMaterial.IRON);
+		goldPaddle = new ItemPaddle(GOLDPADDLE, GOLDPADDLE_NAME, EnumToolMaterial.GOLD);
+		diamondPaddle = new ItemPaddle(DIAMONDPADDLE, DIAMONDPADDLE_NAME, EnumToolMaterial.EMERALD);
+	
 		GameRegistry.addRecipe(new ItemStack(woodHammer), new Object[] { "ccc", "sss", "ccc", Character.valueOf('c'), Block.planks, Character.valueOf('s'), Item.stick });
 		GameRegistry.addRecipe(new ItemStack(cobbleHammer), new Object[] { "ccc", "sss", "ccc", Character.valueOf('c'), Block.cobblestone, Character.valueOf('s'), Item.stick });
 		GameRegistry.addRecipe(new ItemStack(ironHammer), new Object[] { "ccc", "sss", "ccc", Character.valueOf('c'), Item.ingotIron, Character.valueOf('s'), Item.stick });
 		GameRegistry.addRecipe(new ItemStack(goldHammer), new Object[] { "ccc", "sss", "ccc", Character.valueOf('c'), Item.ingotGold, Character.valueOf('s'), Item.stick });
 		GameRegistry.addRecipe(new ItemStack(diamondHammer), new Object[] { "ccc", "sss", "ccc", Character.valueOf('c'), Item.diamond, Character.valueOf('s'), Item.stick });
+		
 		GameRegistry.addRecipe(new ItemStack(fillWand), new Object[] { "s", "s", "s", Character.valueOf('s'), Item.stick });
 
+		
+		GameRegistry.addRecipe(new ItemStack(woodPaddle), new Object[] { "ccc", "s s", "ccc", Character.valueOf('c'), Block.planks, Character.valueOf('s'), Item.stick });
+		GameRegistry.addRecipe(new ItemStack(cobblePaddle), new Object[] { "ccc", "s s", "ccc", Character.valueOf('c'), Block.cobblestone, Character.valueOf('s'), Item.stick });
+		GameRegistry.addRecipe(new ItemStack(ironPaddle), new Object[] { "ccc", "s s", "ccc", Character.valueOf('c'), Item.ingotIron, Character.valueOf('s'), Item.stick });
+		GameRegistry.addRecipe(new ItemStack(goldPaddle), new Object[] { "ccc", "s s", "ccc", Character.valueOf('c'), Item.ingotGold, Character.valueOf('s'), Item.stick });
+		GameRegistry.addRecipe(new ItemStack(diamondPaddle), new Object[] { "ccc", "s s", "ccc", Character.valueOf('c'), Item.diamond, Character.valueOf('s'), Item.stick });
+		
 	}
 }
